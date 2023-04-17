a0e61d4831fbe92f7630e384b9fecc17e9c02e4e
efrael <efrael2001@gmail.com>
2023-04-17 15:58:42 +0200

    s

---
+++ logica/conexion.php
@@ -0,0 +1,19 @@
+<?php 
+   /*
+   $host = "localhost";
+   $user = "u960900126_triviaeuro";
+   $pass = "Cocorilow.1";
+   $database = "u960900126_eurotrivia";
+   $dblink = mysqli_connect($host, $user, $pass, $database);
+
+   if($dblink){
+      }else{echo "NO ESTA CONECTADO";
+   }
+   */
+   $host = "localhost";
+   $user = "root";
+   $pass = "";
+   $database = "liquimolidb";
+   $dblink = mysqli_connect($host, $user, $pass, $database);
+
+?>
